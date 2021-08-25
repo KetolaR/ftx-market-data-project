8f864c9582663231b17bb4da89ae473014f87865
KetolaR <ketola.2@osu.edu>
2021-08-25 01:15:57 -0400

    commit

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,35 @@
 # time to gaming
+
+# create SQL tables
+#   - store trade executions (assume multiple trading pairs and exchanges)
+#   - store aggregated historical trade data, "candles"
+
+
+# fetch historical data for...
+#   - every minute
+#   - hour
+#   - day
+
+#   - save that^ data to SQL table #2
+
+
+# plug in to websocket
+#   - collect trade executions and store in SQL table #1, creating candles
+#       - minutely, hourly, and daily
+#   - run comparison of created candles to new rest API candles
+#       - log deltas
+
+
+# what is open interest?
+
+
+
+# websocket practice
+
+import websocket
+
+
+
+
+# sql practice
+
